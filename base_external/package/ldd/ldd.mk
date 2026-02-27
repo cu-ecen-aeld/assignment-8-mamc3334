87e5bb2bb1d560176524019b163ed55ac7c63caa
Mason McGaffin <mamc3334@colorado.edu>
2026-02-27 11:32:37 -0700

    update a7 ref commit

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,11 +6,11 @@
 ##############################################################
 
 #Reference assignment 7 git contents
-LDD_VERSION = 
+LDD_VERSION = 78c6dc6c7462ff92b92e3f51cd24314fc4b2f52c
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-and-later-mamc3334.git
+LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-mamc3334.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
