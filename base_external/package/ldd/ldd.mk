1ff7de60bcfb346d43bc45ee12d49ff43fbe2922
Mason McGaffin <mamc3334@colorado.edu>
2026-02-28 10:21:18 -0700

    assignment7-part2 complete

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -13,19 +13,16 @@ LDD_VERSION = 78c6dc6c7462ff92b92e3f51cd24314fc4b2f52c
 LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-mamc3334.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
+LDD_MODULE_SUBDIRS = scull misc-modules
 
-define LDD_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/misc-modules all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/scull all
-endef
-
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-# TODO add server application
+# add load and unload scripts for modules
 define LDD_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/init.d/
 	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/etc/init.d/module_load
 	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/etc/init.d/module_unload
 	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/etc/init.d/scull_load
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/etc/init.d/scull_unload
 endef
 
+$(eval $(kernel-module))
 $(eval $(generic-package))
